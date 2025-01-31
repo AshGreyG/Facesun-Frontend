86e6a0f9799e324cc3e398d896f898fe8defc231
AshGreyG <ashgrey.huaier@gmail.com>
2025-01-31 22:53:55 +0800

    feat : test the axios to the backend

--- frontend/src/components/SwitchLanguageBar.tsx
+++ frontend/src/components/SwitchLanguageBar.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 import "./SwitchLanguageBar.css"
 
 function SwitchLanguageBar() {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
   const handleSwitchLanguage = () => {
     if (i18n.language === "en") {
       i18n.changeLanguage("zh");
--- frontend/src/page/LoginPage.tsx
+++ frontend/src/page/LoginPage.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import axios from "axios";
 
 import "./LoginPage.css";
 import AnimatedLabel from "../components/AnimatedLabel.tsx";
 import SwitchLanguageBar from "../components/SwitchLanguageBar.tsx";
+import backendURL from "../utility/backendURL.tsx";
 
 function LoginPage() {
+  const loginAPI = axios.create({
+    baseURL: backendURL,
+    timeout: 5000,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
   const { t } = useTranslation();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -20,12 +29,19 @@ function LoginPage() {
 
     // TODO: Need post to backend to check the username and password
 
-    if (username === "admin" && password === "test") {
-      // navigate("/login");
-      alert("Login Success")
-    } else {
-      alert("Login Failed");
-    }
+    loginAPI
+      .post("/manager/login", {
+        "username": username,
+        "password": password
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        alert(error);
+      })
+
+
   };
 
   const handleRegister = (event) => {
